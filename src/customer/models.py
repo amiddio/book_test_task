3f876ae3bb55485b2d15aec04ba7b930866df78d
d.shkinin <amiddio.dev@gmail.com>
2023-11-27 17:26:49 +0400

    Created migration for model Customer

--- src/customer/models.py
+++ src/customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    """Модель пользователя"""
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
