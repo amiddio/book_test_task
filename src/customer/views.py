fb94b0136e11d4914f1c8fbbaac8ff27bd1bc2da
d.shkinin <amiddio.dev@gmail.com>
2023-11-27 18:38:19 +0400

    Created customer view, serializer and saving feature

--- src/customer/views.py
+++ src/customer/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import CustomerSerializer
+
+
+class CustomerAPIView(APIView):
+    """Представление для создания пользователей"""
+
+    def post(self, request):
+        serializer = CustomerSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # serializer.save()
+
+        return Response({'result': True}, status=status.HTTP_201_CREATED)
+
