983dc15948f30b56fe40319fb47a44870f76eea6
d.shkinin <amiddio.dev@gmail.com>
2023-11-28 12:58:45 +0400

    Added celery task after customer registered

--- src/customer/views.py
+++ src/customer/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import CustomerSerializer
+from .tasks import send_customer_register_email_task
 
 
 class CustomerAPIView(APIView):
@@ -11,7 +12,10 @@ class CustomerAPIView(APIView):
     def post(self, request):
         serializer = CustomerSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # serializer.save()
+        data = serializer.validated_data
+        serializer.save()
+
+        # Асинхронная задача для отправки email-а
+        send_customer_register_email_task.delay(data=data)
 
         return Response({'result': True}, status=status.HTTP_201_CREATED)
-
